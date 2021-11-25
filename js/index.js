37cd7f0a2aed0c3ef892a584efd9b03d6692d239
michaellosdev <mishaloskutov@gmail.com>
2021-11-24 17:40:58 -0800

    changed style a bit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// variables 
+
+//const containerTwo = document.getElementById("container-two");
+//const webDesignProcess = document.getElementsByClassName("web-design-process")[0];
+
+
+//containerTwo.addEventListener('scroll', function() {
+    //webDesignProcess.style.opacity = '1'
+//})
+
+
+
+
+
