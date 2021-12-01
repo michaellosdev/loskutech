9710994cde6f061803151088e6265ecebd7c0455
michaellosdev <mishaloskutov@gmail.com>
2021-11-30 18:58:58 -0800

    added hamburger menu, screen <= 600px

--- js/index.js
+++ js/index.js
@@ -17,12 +17,41 @@ closeModal.addEventListener('click', function() {
 
 //mobile menu 
 
+
+
+const body = document.getElementsByTagName('body')[0]; 
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 
+//mobile menu open/close button
 
+const firstLine = document.getElementById('first-line');
+const secondLine = document.getElementById('second-line');
+const thirdLine = document.getElementById('third-line');
+
+
+// on click event listener
 
 mobileMenuBtn.addEventListener('click', ()=> {
-    mobileMenu.style.display = "block"
+
+    if (mobileMenu.style.display === "block") {
+            firstLine.style.animation = 'first-line-out 0.2s linear 1 forwards';
+            secondLine.style.animation = 'second-line-out 0.2s linear 1 forwards';
+            thirdLine.style.animation = 'third-line-out 0.2s linear 1 forwards';
+            mobileMenu.style.animation = 'mobile-menu-close 0.2s linear 1 forwards';
+            setTimeout(function() {mobileMenu.style.display = "none"},200);
+    } else  {
+            mobileMenu.style.display = 'block'
+            mobileMenu.style.animation= 'mobile-menu-open 0.2s linear 1 forwards'
+            firstLine.style.animation = 'first-line 0.2s linear 1 forwards'
+            secondLine.style.animation = 'second-line 0.2s linear 1 forwards'
+            thirdLine.style.animation = 'third-line 0.2s linear 1 forwards'
+    }
+
 })
+
+
+
+
+
 
